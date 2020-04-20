4b6132be130c663837f07f51a1f0c420c6df38c3
ijvitaga <63268874+ijvitaga@users.noreply.github.com>
2020-04-19 21:52:43 -0700

    Add files via upload

---
+++ memory_game/js/main.js
@@ -0,0 +1,45 @@
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+const cardsInPlay = ["queen", "queen", "king", "king"];
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+function flipCard(cardID) {
+	console.log("User flipped " + cards[cardID].rank);
+}
+flipCard(0);
+flipCard(2);
+cardsInPlay.push(cards[cardID])
+function createBoard(){
+	for (let i = 0; i < cardsInPlay.length; i++) {
+    let cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+}
+
+
